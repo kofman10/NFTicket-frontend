27248326e2b70351a954942c5d50d2307ee0a6f4
kofman10 <kajotonis@gmal.com>
2023-08-25 16:55:46 +0100

    final push

--- src/pages/events.js
+++ src/pages/events.js
@@ -85,7 +85,7 @@ const events = () => {
           ))}
         </div>
       ) : (
-        <p> Connect your wallet to Base-goerli testnet chain</p>
+        <p className="text-center text-5xl text-white"> Connect your wallet to Base-goerli testnet chain</p>
       )}
     </div>
   );
